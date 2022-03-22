dc33eeb95396db7611aa4b8a3ebf276bce892f9a
Tadesse-Alemayehu <email24t@gmail.com>
2022-03-22 09:17:18 +0300

    \configure webpack for css files

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,14 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
