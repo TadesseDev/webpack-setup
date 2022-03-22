eba393e7aae3163a2ac03246456ced693dc6f7aa
Tadesse-Alemayehu <email24t@gmail.com>
2022-03-22 09:49:35 +0300

    configure live server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
+  mode: 'production',
+  entry: {
+    js: './src/index.js',
+  },
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
